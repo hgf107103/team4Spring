5dc2e0499975932e3b974824a73eed0d79eaa473
sakurai_sakura <hgf107103@gmail.com>
2020-12-17 17:56:13 +0900

    2020 12 17 17:56

---
+++ src/main/java/com/team4/travel/object/bookmarkVO.java
@@ -0,0 +1,17 @@
+package com.team4.travel.object;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class bookmarkVO {
+	private int areaNumber;
+	private int countryNumber;
+	private String countryName;
+	private String englishName;
+	private String koreanName;
+	private int userNumber;
+}
